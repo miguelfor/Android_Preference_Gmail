c1923298aa8e4fc99a55387c31f9fcfa73dfe887
“MiguelFor” <miguelfor@hotmail.com>
2018-10-01 12:05:10 -0500

    First commit

---
+++ app/src/main/java/com/nextuniversity/configuraciongmail/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nextuniversity.configuraciongmail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
